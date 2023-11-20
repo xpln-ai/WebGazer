456e42417631da02a688387adea7e835d9667d9c
Cideck <cideck@gmail.com>
2023-11-20 11:27:11 +0100

    prevent creation of default face tracker TFFaceMesh

--- src/index.mjs
+++ src/index.mjs
@@ -57,7 +57,9 @@ var eventTypes = ['click', 'move'];
 //movelistener timeout clock parameters
 var moveClock = performance.now();
 //currently used tracker and regression models, defaults to clmtrackr and linear regression
-var curTracker = new webgazer.tracker.TFFaceMesh();
+// xpln.ai 2023-11-19: Disable default TFFaceMesh tracker, because we use our own faceTracker!
+
+var curTracker = null; // new webgazer.tracker.TFFaceMesh();
 var regs = [new webgazer.reg.RidgeReg()];
 // var blinkDetector = new webgazer.BlinkDetector();
 
